034acd07a8a24b268850c3ef689c0568b49b8c6a
Valentin181988 <85246610+Valentin181988@users.noreply.github.com>
2022-07-02 12:48:41 +0200

    publications api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import {Routes, Route} from 'react-router-dom';
 import {Layout} from './components/Layout';
+import {ListPage} from './pages';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
          <Route path="preview" element={<div>Preview page</div>}/>
-         <Route path="list" element={<div>List page</div>}/>
+         <Route path="list" element={<ListPage />}/>
          <Route path="create" element={<div>Create page</div>}/>
       </Route>
     </Routes>
---
+++ src/pages/ListPage.jsx
@@ -0,0 +1,5 @@
+export const ListPage = () => {
+    return(
+        <main>List Page</main>
+    );
+};
