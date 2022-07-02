70ec029aab75c034be720fca84a00636f41d8ef9
Valentin181988 <85246610+Valentin181988@users.noreply.github.com>
2022-07-02 21:16:02 +0200

    api

--- src/pages/ListPage.jsx
+++ src/pages/ListPage.jsx
@@ -1,5 +1,24 @@
+import {useEffect, useState} from 'react';
+import {getPublications} from '../servises/publicationsApi';
+
 export const ListPage = () => {
+
+    const [items, setItems] = useState([]);
+
+    useEffect(() => {
+        getPublications().then(setItems);
+    }, []);
+
+    console.log(items)
+
     return(
-        <main>List Page</main>
+        <main>
+            <h1>List Page</h1>
+            <ol>
+              {items.map(item => (
+                 <li key={item.id}>{item.title}</li> 
+              ))} 
+            </ol>     
+        </main>
     );
 };
--- src/servises/publicationsApi.js
+++ src/servises/publicationsApi.js
@@ -1,19 +1,19 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://61e9af6e7bc0550017bc6417.mockapi.io';
+axios.defaults.baseURL = 'https://62c094d7c134cf51ced178f4.mockapi.io/';
 
 export const getPublications = async () => {
-    const response = await axios.get('/publications');
+    const response = await axios.get('/articles');
     return response.data;
 };
 
 export const addPublication = async data => {
-    const response = await axios.post('/publications', data);
+    const response = await axios.post('/articles', data);
     return response.data;
 };
 
 export const deletePublication = async id => {
-    const response = await axios.delete(`/publications/${id}`);
+    const response = await axios.delete(`/articles/${id}`);
     return response.data;
 };
 
