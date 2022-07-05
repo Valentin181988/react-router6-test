ce064b5c9e180dd507a71a4b3210cd23fcc52ad2
Valentin181988 <85246610+Valentin181988@users.noreply.github.com>
2022-07-05 14:48:23 +0200

    searchParams

--- src/pages/PageA.jsx
+++ src/pages/PageA.jsx
@@ -1,13 +1,22 @@
+import { useEffect } from "react";
 import { Link, useSearchParams, useLocation } from "react-router-dom";
 
 export const PageA = () => {
     const location = useLocation();
     const [searchParams, setSearchParams] = useSearchParams();
 
+    const query = searchParams.get('query');
+
+    useEffect(() => {
+        if (query) {
+            console.log('Make the request')
+        }
+    }, [query]);
+
     const handleSubmit = e => {
         e.preventDefault();
 
-        setSearchParams({qery: e.currentTarget.elements.qery.value});
+        setSearchParams({query: e.currentTarget.elements.qery.value});
        };
 
     return (
